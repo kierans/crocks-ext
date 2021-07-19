da5655625f644e1062506dfa6f44452f73c472e8
Kieran Simpson <kierans777@gmail.com>
2021-07-19 11:13:11 +1000

    Add string utilities.

---
+++ src/String/index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const Endo = require("crocks/Endo");
+
+const binary = require("crocks/helpers/binary");
+const compose = require("crocks/helpers/compose");
+const curry = require("crocks/helpers/curry");
+const fanout = require("crocks/Pair/fanout");
+const head = require("crocks/pointfree/head");
+const map = require("crocks/pointfree/map");
+const merge = require("crocks/pointfree/merge");
+const mreduce = require("crocks/helpers/mreduce");
+const option = require("crocks/pointfree/option");
+const pipe = require("crocks/helpers/pipe");
+const tail = require("crocks/pointfree/tail");
+
+const { prepend, zipArgs } = require("../helpers");
+
+// capitalise :: String -> String
+const capitalise = (str) => `${str[0].toUpperCase()}${str.substring(1)}`
+
+// join :: String -> [ String ] -> String
+const join = curry((sep, arr) => arr.join(sep))
+
+// split :: String -> String -> [ String ]
+const split = curry((sep, str) => str.split(sep))
+
+// lowerCase :: String -> String
+const lowerCase = (str) => str.toLowerCase()
+
+// upperCase :: String -> String
+const upperCase = (str) => str.toUpperCase()
+
+// toCamelCase :: [ String ] -> String
+const toCamelCase =
+	pipe(
+		fanout(head, tail),
+		map(map(compose(join(""), map(capitalise)))),
+		merge(prepend),
+		option("")
+	)
+
+// toKebabCase :: [ String ] -> String
+const toKebabCase =
+	pipe(
+		map(lowerCase),
+		join("-")
+	)
+
+// changeCase :: (String -> [ String ]) -> ([ String ] -> String) -> String -> String
+const changeCase = binary(compose(mreduce(Endo), zipArgs))
+
+// fromKebabCase
+const fromKebabCase = split("-")
+
+module.exports = {
+	capitalise,
+	changeCase,
+	fromKebabCase,
+	join,
+	lowerCase,
+	split,
+	toCamelCase,
+	toKebabCase,
+	upperCase
+}
---
+++ test/String/index.test.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const { assertThat, is } = require("hamjest");
+
+const {
+	capitalise,
+	changeCase,
+	fromKebabCase,
+	join,
+	lowerCase,
+	split,
+	toCamelCase,
+	toKebabCase,
+	upperCase
+} = require("../../src/String");
+
+describe("String", function() {
+	it("should capitalise word", function() {
+		assertThat(capitalise("hello"), is("Hello"));
+	})
+
+	it("should join word", function() {
+		assertThat(join("-", [ "a", "b", "c" ]), is("a-b-c"))
+	});
+
+	it("should split word", function() {
+		assertThat(split("-", "a-b-c"), is([ "a", "b", "c" ]))
+	});
+
+	it("should lower case word", function() {
+		assertThat(lowerCase("HELLO"), is("hello"))
+	});
+
+	it("should upper case word", function() {
+		assertThat(upperCase("hello"), is("HELLO"))
+	});
+
+	describe("camelCase", function() {
+		it("should camel case words", function() {
+			assertThat(toCamelCase([ "a", "var" ]), is("aVar"))
+		});
+
+		it("should return blank string for empty list", function() {
+			assertThat(toCamelCase([]), is(""));
+		})
+	});
+
+	describe("kebab case", function() {
+		it("should kebab case words", function() {
+			assertThat(toKebabCase([ "A", "VAR" ]), is("a-var"));
+		});
+
+		it("should return blank string for empty list", function() {
+			assertThat(toKebabCase([]), is(""));
+		})
+	});
+
+	it("should change case", function() {
+		assertThat(changeCase(fromKebabCase, toCamelCase)("a-var"), is("aVar"));
+	});
+});
