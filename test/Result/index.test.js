913afbe7e7386b7005adbd8b475f22635b495c64
Kieran Simpson <kierans777@gmail.com>
2021-07-19 14:37:24 +1000

    Reorg tests

--- test/Result/index.test.js
+++ test/Result/index.test.js
@@ -10,82 +10,84 @@ const { getProp, getPath } = require("../../src/Result");
 const { throwError } = require("../../src/utils");
 
 describe("Result", function() {
-	describe("object props", function() {
-		const data = [
-			{
-				id: "getProp",
-				fn: getProp,
-				good: "name",
-				bad: "foo",
-				data: {
-					name: "Batman"
-				}
-			},
-			{
-				id: "getPath",
-				fn: getPath,
-				good: [ "name", "first" ],
-				bad: [ "foo" ],
-				data: {
-					name: {
-						first: "Bruce",
-						last: "Wayne"
+	describe("getOrError", function() {
+		describe("object props", function() {
+			const data = [
+				{
+					id: "getProp",
+					fn: getProp,
+					good: "name",
+					bad: "foo",
+					data: {
+						name: "Batman"
+					}
+				},
+				{
+					id: "getPath",
+					fn: getPath,
+					good: [ "name", "first" ],
+					bad: [ "foo" ],
+					data: {
+						name: {
+							first: "Bruce",
+							last: "Wayne"
+						}
 					}
 				}
-			}
-		];
+			];
 
-		runTest(data)
-	});
+			runTest(data)
+		});
 
-	describe("array index", function() {
-		const data = [
-			{
-				id: "getProp",
-				fn: getProp,
-				good: 0,
-				bad: 1,
-				data: [
-					"Batman"
-				]
-			},
-			{
-				id: "getPath",
-				fn: getPath,
-				good: [ 0, 0 ],
-				bad: [ 0, 1 ],
-				data: [
-					[
+		describe("array index", function() {
+			const data = [
+				{
+					id: "getProp",
+					fn: getProp,
+					good: 0,
+					bad: 1,
+					data: [
 						"Batman"
 					]
-				]
-			}
-		];
+				},
+				{
+					id: "getPath",
+					fn: getPath,
+					good: [ 0, 0 ],
+					bad: [ 0, 1 ],
+					data: [
+						[
+							"Batman"
+						]
+					]
+				}
+			];
 
-		runTest(data)
-	});
+			runTest(data)
+		});
 
-	function runTest(data) {
-		data.forEach((datum) => {
-			describe(datum.id, function() {
-				const err = new Error("fake error");
-				const illegalCall = () => { throw new Error("Error factory called"); }
+		function runTest(data) {
+			data.forEach((datum) => {
+				describe(datum.id, function() {
+					const err = new Error("fake error");
+					const illegalCall = () => { throw new Error("Error factory called"); }
 
-				it("should return value when found", function() {
-					// this also tests that the error is evaluated lazily
-					const result = datum.fn(illegalCall, datum.good, datum.data);
+					it("should return value when found", function() {
+						// this also tests that the error is evaluated lazily
+						const result = datum.fn(illegalCall, datum.good, datum.data);
 
-					result.either(throwError, identity);
-				});
+						result.either(throwError, identity);
+					});
 
-				it("should return error when value not found", function() {
-					const result = datum.fn(constant(err), datum.bad, datum.data);
+					it("should return error when value not found", function() {
+						const result = datum.fn(constant(err), datum.bad, datum.data);
 
-					const e = result.either(identity, throwError);
+						const e = result.either(identity, throwError);
 
-					assertThat(e, is(err));
+						assertThat(e, is(err));
+					});
 				});
 			});
-		});
-	}
+		}
+	});
 });
