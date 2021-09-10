57267b24115916c2e834894ee01a3c979cba8bb3
Kieran Simpson <kierans777@gmail.com>
2021-09-10 12:02:23 +1000

    Create directories when writing file.

--- src/node/fs.js
+++ src/node/fs.js
@@ -1,13 +1,18 @@
 "use strict";
 
 const fs = require("fs");
+const path = require("path");
 
 const Async = require("crocks/Async");
 
+const bichain = require("crocks/pointfree/bichain");
 const compose = require("crocks/helpers/compose");
+const constant = require("crocks/combinators/constant");
 const chain = require("crocks/pointfree/chain");
 const curry = require("crocks/core/curry");
+const ifElse = require("crocks/logic/ifElse");
 const pipe = require("crocks/helpers/pipe");
+const pipeK = require("crocks/helpers/pipeK");
 const resultToAsync = require("crocks/Async/resultToAsync");
 
 const { parse} = require("./json");
@@ -16,11 +21,37 @@ const readAsync = Async.fromNode(fs.readFile);
 
 const writeAsync = Async.fromNode(fs.writeFile);
 
+const mkdirAsync = Async.fromNode(fs.mkdir);
+
+// dirname :: String -> String
+const dirname = (file) => path.dirname(file)
+
 // readFile :: Object -> (String | Buffer | URL | Integer) -> Async Error a
 const readFile = curry((opts, path) => readAsync(path, opts))
 
-// writeFile :: Object -> (String | Buffer | URL | Integer) -> (String | Buffer | TypedArray | DataView | Object) -> Async Error a
-const writeFile = curry((opts, path, data) => writeAsync(path, data, opts))
+// ignoreExistingDirectory :: Error -> Async Error undefined
+const ignoreExistingDirectory =
+	ifElse(
+		({ code }) => code === "EEXIST",
+		() => Async.Resolved(),
+		Async.Rejected
+	)
+
+// mkdirsAsync :: (String | Buffer | URL | Integer) -> Async Error undefined
+const mkdirsAsync =
+	pipe(
+		dirname,
+		(dir) => mkdirAsync(dir, { recursive: true }),
+		bichain(ignoreExistingDirectory, Async.Resolved)
+	)
+
+// writeFile :: Object -> (String | Buffer | URL | Integer) -> (String | Buffer | TypedArray | DataView | Object) -> Async Error undefined
+const writeFile = curry((opts, path, data) =>
+	pipeK(
+		constant(mkdirsAsync(path)),
+		() => writeAsync(path, data, opts)
+	)()
+)
 
 // readJSON :: (String | Buffer | URL | Integer) -> Async Error a
 const readJSON =
--- test/node/fs.test.js
+++ test/node/fs.test.js
@@ -2,25 +2,58 @@
 
 const fs = require("fs");
 const os = require("os");
+const path = require("path");
+
+const rimraf = require("rimraf");
 
 const { assertThat, is } = require("hamjest");
 
-const { readJSON } = require("../../src/node/fs");
+const { readJSON, writeFile } = require("../../src/node/fs");
 
 describe("fs", function() {
 	describe("readJSON", function() {
+		const filename = `${os.tmpdir()}/fs.test.json`;
+
+		beforeEach(rmFile(filename))
+		afterEach(rmFile(filename))
+
 		it("should read json", async function() {
-			const filename = `${os.tmpdir()}/fs.test.json`;
 			fs.writeFileSync(filename, '{ "a": 1 }')
 
-			try {
-				const result = await readJSON(filename).toPromise();
+			const result = await readJSON(filename).toPromise();
 
-				assertThat(result.a, is(1));
-			}
-			finally {
-				fs.unlinkSync(filename);
-			}
+			assertThat(result.a, is(1));
 		});
 	});
+
+	describe("writeFile", function() {
+		const writeStringFile = writeFile({ encoding: "utf8" });
+
+		const filename = `${os.tmpdir()}/a/b/c/fs.test.json`
+
+		beforeEach(rmFile(`${os.tmpdir()}/a`))
+		afterEach(rmFile(`${os.tmpdir()}/a`))
+
+		it("should create directories when saving file", async function() {
+			await writeStringFile(filename, '{ "a": 1 }').toPromise()
+
+			await fs.promises.stat(filename);
+		});
+
+		it("should reuse existing directories when saving file", async function() {
+			const dirs = path.dirname(filename);
+			await fs.promises.mkdir(dirs, { recursive: true });
+
+			await writeStringFile(filename, '{ "a": 1 }').toPromise()
+
+			await fs.promises.stat(filename);
+		});
+
+	});
+
+	function rmFile(filename) {
+		return async function() {
+			await rimraf.sync(filename);
+		}
+	}
 });
