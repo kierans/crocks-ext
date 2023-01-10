49f8686840c02aae7d515a55e57d20a2fc600288
Kieran Simpson <kierans777@gmail.com>
2023-01-10 14:24:42 +1100

    Add additional zip functions.

--- src/helpers/zip.js
+++ src/helpers/zip.js
@@ -1,9 +1,31 @@
 "use strict";
 
+const Min = require("crocks/Min");
+const Pair = require("crocks/Pair");
+
+const applyTo = require("crocks/combinators/applyTo");
+const compose = require("crocks/helpers/compose");
+const constant = require("crocks/combinators/constant");
+const curry = require("crocks/helpers/curry");
+const getPropOr = require("crocks/helpers/getPropOr");
+const flip = require("crocks/combinators/flip");
+const map = require("crocks/pointfree/map");
+const mreduce = require("crocks/helpers/mreduce");
+const nAry = require("crocks/helpers/nAry");
+const reduce = require("crocks/pointfree/reduce");
+
+const { arrayToTuple } = require("../Tuple");
+const { length } = require("./lists");
+const { collectLoop, mapLoop } = require("./loops");
+
+// lookup :: [ a ] -> Integer -> a
+const lookup =
+	flip(getPropOr(undefined))
+
 /*
  * Collects the arguments to a function into an array
  */
-// zipArgs :: * -> [ * ]
+// zipArgs :: (*) -> [ * ]
 const zipArgs = function() {
 	/*
 	 * Can't be an arrow function because of the way Node args work.
@@ -11,6 +33,46 @@ const zipArgs = function() {
 	return Array.prototype.slice.call(arguments)
 }
 
+/*
+ * zipTo takes a n-arity producer function and a 2D array.
+ *
+ * It then applies the producer function to the n-th element of every child array, until
+ * a final result is produced.
+ *
+ * The returned array is truncated to the length of the shortest of the child arrays.
+ */
+// zipTo :: ((*) -> b) -> [ [ a ] ] -> [ b ]
+const zipTo = (fn) => (...arrs) => {
+	const min = mreduce(Min, map(length, arrs));
+	const collectors = collectLoop(min, constant(nAry(length(arrs), fn)))
+
+	// applyCollector :: [ (*) -> b ] -> a -> Integer -> ((*) -> b | b)
+	const applyCollector = curry((collectors, el) =>
+		compose(applyTo(el), lookup(collectors))
+	)
+
+	return reduce(
+		curry(compose(mapLoop(min), applyCollector)),
+		collectors,
+		arrs
+	)
+}
+
+const zipToArray = zipTo(zipArgs)
+
+const zipToPair = (...arrs) => {
+	if (arrs.length !== 2) {
+		throw new TypeError("zipToPair: requires two arrays");
+	}
+
+	return zipTo(curry(Pair))(...arrs);
+}
+
+const zipToTuple = compose(map(arrayToTuple), zipToArray)
+
 module.exports = {
-	zipArgs
+	zipArgs,
+	zipToArray,
+	zipToPair,
+	zipToTuple
 }
--- test/helper/zip.test.js
+++ test/helper/zip.test.js
@@ -1,12 +1,28 @@
 "use strict";
 
-const { assertThat, is } = require("hamjest");
+const { tupleToArray } = require("crocks");
 
 const {
-	zipArgs
+	allOf,
+	assertThat,
+	equalTo,
+	hasProperty,
+	instanceOf,
+	is,
+	throws
+} = require("hamjest");
+
+const {
+	zipArgs,
+	zipToArray,
+	zipToPair,
+	zipToTuple
 } = require("../../src/helpers/zip");
 
-describe("zip helps", function() {
+// pairToArray :: Pair a b -> [ a, b ]
+const pairToArray = (p) => p.toArray()
+
+describe("zip helpers", function() {
 	describe("zipArgs", function() {
 		it("should collect args", function() {
 			const one = () => 1
@@ -21,4 +37,122 @@ describe("zip helps", function() {
 			assertThat(result[2], is(three));
 		})
 	});
+
+	describe("zipToArray", function() {
+		it("should zip to array", function() {
+			const result = zipToArray(
+				[ 1, 4, 7 ],
+				[ 2, 5, 8 ],
+				[ 3, 6, 9 ]
+			);
+
+			assertThat(result, is(equalTo([
+				[ 1, 2, 3 ],
+				[ 4, 5, 6 ],
+				[ 7, 8, 9 ]
+			])))
+		});
+
+		it("should zip to length of shortest array", function() {
+			const result = zipToArray(
+				[ 1, 4, 7 ],
+				[ 2, 5 ],
+				[ 3, 6, 9 ]
+			);
+
+			assertThat(result, is(equalTo([
+				[ 1, 2, 3 ],
+				[ 4, 5, 6 ]
+			])))
+		});
+	});
+
+	describe("zipToTuple", function() {
+		it("should zip to tuple", function() {
+			const result =
+				zipToTuple(
+					[ 1, 4, 7 ],
+					[ 2, 5, 8 ],
+					[ 3, 6, 9 ]
+				)
+				.map(tupleToArray);
+
+			assertThat(result, is(equalTo([
+				[ 1, 2, 3 ],
+				[ 4, 5, 6 ],
+				[ 7, 8, 9 ]
+			])));
+		});
+
+		it("should zip to length of shortest array", function() {
+			const result =
+				zipToTuple(
+					[ 1, 4, 7 ],
+					[ 2, 5 ],
+					[ 3, 6, 9 ]
+				)
+				.map(tupleToArray);
+
+			assertThat(result, is(equalTo([
+				[ 1, 2, 3 ],
+				[ 4, 5, 6 ]
+			])))
+		});
+	});
+
+	describe("zipToPair", function() {
+		it("should zip to pair", function() {
+			const result =
+				zipToPair(
+					[ 1, 4, 7 ],
+					[ 2, 5, 8 ]
+				)
+				.map(pairToArray);
+
+			assertThat(result, is(equalTo([
+				[ 1, 2 ],
+				[ 4, 5 ],
+				[ 7, 8 ]
+			])))
+		});
+
+		it("should throw error is array length too short", function() {
+			const fn = () => zipToPair(
+				[ 1, 4, 7 ]
+			);
+
+			assertThat(fn, throws(typeError()))
+		});
+
+		it("should throw error is array length too big", function() {
+			const fn = () => zipToPair(
+				[ 1, 4, 7 ],
+				[ 2, 5, 8 ],
+				[ 3, 6, 9 ]
+			);
+
+			assertThat(fn, throws(typeError()))
+		});
+
+		it("should zip to length of shortest array", function() {
+			const result =
+				zipToPair(
+					[ 1, 4, 7 ],
+					[ 2, 5 ]
+				)
+				.map(pairToArray);
+
+			assertThat(result, is(equalTo([
+				[ 1, 2 ],
+				[ 4, 5 ]
+			])))
+		});
+	});
 });
+
+function typeError() {
+	return allOf(
+		instanceOf(TypeError),
+		hasProperty("message", "zipToPair: requires two arrays")
+	)
+}
