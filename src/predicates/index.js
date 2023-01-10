9d59e338bc98a55b24959060097930ec6d2e63b4
Kieran Simpson <kierans777@gmail.com>
2023-01-10 13:23:47 +1100

    Add number comparison predicates.

--- src/predicates/index.js
+++ src/predicates/index.js
@@ -2,9 +2,25 @@
 
 const curry = require("crocks/helpers/curry");
 
+// isGreaterThan :: Number -> Number -> Boolean
+const isGreaterThan = curry((a, b) => b > a)
+
+// isGreaterThanEqualTo :: Number -> Number -> Boolean
+const isGreaterThanEqualTo = curry((a, b) => b >= a)
+
+// isLessThan :: Number -> Number -> Boolean
+const isLessThan = curry((a, b) => b < a)
+
+// isLessThanEqualTo :: Number -> Number -> Boolean
+const isLessThanEqualTo = curry((a, b) => b <= a)
+
 // matchesRegex :: RegExp -> String -> Boolean
 const matchesRegex = curry((regexp, str) => regexp.test(str))
 
 module.exports = {
+	isGreaterThan,
+	isGreaterThanEqualTo,
+	isLessThan,
+	isLessThanEqualTo,
 	matchesRegex
 }
--- test/predicates/index.test.js
+++ test/predicates/index.test.js
@@ -2,12 +2,42 @@
 
 const { assertThat, is } = require("hamjest");
 
-const { matchesRegex } = require("../../src/predicates");
+const {
+	isGreaterThan,
+	isGreaterThanEqualTo,
+	isLessThan,
+	isLessThanEqualTo,
+	matchesRegex
+} = require("../../src/predicates");
 
 describe("predicates", function() {
 	describe("matchesRegexp", function() {
 		it("should match regexp", function() {
 			assertThat(matchesRegex(/\d{4}/, 2021), is(true));
+		});
+	});
+
+	describe("comparison predicates", function() {
+		it("should compare greaterThan", function() {
+			assertThat(isGreaterThan(3, 2), is(false));
+			assertThat(isGreaterThan(2, 3), is(true));
+		});
+
+		it("should compare greaterThanEqualTo", function() {
+			assertThat(isGreaterThanEqualTo(3, 2), is(false));
+			assertThat(isGreaterThanEqualTo(2, 3), is(true));
+			assertThat(isGreaterThanEqualTo(2, 2), is(true));
+		});
+
+		it("should compare lessThan", function() {
+			assertThat(isLessThan(3, 2), is(true));
+			assertThat(isLessThan(2, 3), is(false));
+		});
+
+		it("should compare lessThanEqualTo", function() {
+			assertThat(isLessThanEqualTo(3, 2), is(true));
+			assertThat(isLessThanEqualTo(2, 3), is(false));
+			assertThat(isLessThanEqualTo(2, 2), is(true));
 		});
 	});
 });
