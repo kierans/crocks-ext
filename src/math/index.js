f1697f45ddc3801f50884da2b89a511fc3b6dbc0
Kieran Simpson <kierans777@gmail.com>
2023-07-09 20:36:48 +1000

    Add mod function

--- src/math/index.js
+++ src/math/index.js
@@ -8,7 +8,11 @@ const add = curry((a, b) => a + b)
 // inc :: Number -> Number
 const inc = add(1)
 
+// mod :: Number -> Number -> Number
+const mod = curry((a, b) => b % a)
+
 module.exports = {
 	add,
-	inc
+	inc,
+	mod
 }
