9c2225c08d16421c801e1b7b73942d596e8ed5ea
Kieran Simpson <kierans777@gmail.com>
2023-10-11 11:47:25 +1100

    Improve readability.

--- src/pointfree/reduceWhile.js
+++ src/pointfree/reduceWhile.js
@@ -9,21 +9,20 @@ const head = require("crocks/pointfree/head");
 const ifElse = require("crocks/logic/ifElse");
 const option = require("crocks/pointfree/option");
 const map = require("crocks/pointfree/map");
-const nAry = require("crocks/helpers/nAry");
 const tail = require("crocks/pointfree/tail");
-
-// trinary :: a -> b -> c -> d
-const trinary = nAry(3);
 
 // rest :: Foldable f => f a -> f a
 const rest = compose(option([]), tail)
 
-// ifHead :: Functor m => (b -> Boolean) -> (b -> (f b -> a) -> (b -> (f b -> a)) -> m b -> m (f b -> a)
-const ifHead = trinary(compose(map, ifElse))
+// ifHead :: (b -> Boolean) -> (b -> (f b -> a) -> (b -> (f b -> a)) -> b -> (f b -> a)
+const ifHead = ifElse
 
-// reduceHead :: Foldable f => (b -> Boolean) -> (b -> (f b -> a) -> (f b -> a) -> f b -> (f b -> a)
-const reduceHead = curry((pred, ifTrue, ifFalse) =>
-	compose(option(ifFalse), ifHead(pred, ifTrue, constant(ifFalse)), head)
+// noHead :: a -> (f b -> a)
+const noHead = constant
+
+// reduceHead :: Foldable f => (b -> (f b -> a)) -> (f b -> a) -> f b -> (f b -> a)
+const reduceHead = curry((ifHead, noHead) =>
+	compose(option(noHead), map(ifHead), head)
 )
 
 /*
@@ -43,7 +42,10 @@ const reduceWhile = curry((pred, f, acc) =>
 		 */
 		applyTo,
 		rest,
-		reduceHead(pred(acc), compose(reduceWhile(pred, f), f(acc)), constant(acc))
+		reduceHead(
+			ifHead(pred(acc), compose(reduceWhile(pred, f), f(acc)), constant(constant(acc))),
+			noHead(acc)
+		)
 	)
 )
 
